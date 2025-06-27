28b0bc8e83f396cba8b0e6b3c571848705cdd687
ALaunert <alexmansupwork@gmail.com>
2025-06-27 12:01:57 +0200

    added api location to nginx.conf, new test endpoint, "reload" key in python dockerfile

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello from backend"}
+
+
+@app.get("/api/test")
+def read_root():
+    return {"message": "Hello from test"}
