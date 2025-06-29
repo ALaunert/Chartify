5837a1576e037b4515caee22c06d6173ae699bf3
ALaunert <alexmansupwork@gmail.com>
2025-06-29 17:16:41 +0200

    added new endpoints, to look up info about the artist, and to search the artists

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,27 +1,35 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Path
 from app.service import spotify_token_manager
 import httpx
-import os
 
-if os.environ.get("DEBUG", "0") == "1":
-    import debugpy
-    debugpy.listen(("0.0.0.0", 5678))
-    print("Waiting for debugger attach...")
-    debugpy.wait_for_client()
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from backend"}
 
 
-@app.get("/api/test")
-async def read_root():
+@app.get("/artist/{artist_id}")
+async def read_root(
+        artist_id: Annotated[str, Path(title="Artist ID")],
+):
     token = await spotify_token_manager.get_token()
 
     async with httpx.AsyncClient() as client:
         client.headers["Authorization"] = f"Bearer {token}"
-        response = await client.get('https://api.spotify.com/v1/artists/3YQKmKGau1PzlVlkL1iodx')
+        response = await client.get(f'https://api.spotify.com/v1/artists/{artist_id}')
+
+    return response.json()
+
+@app.get("/artist/search/{query}")
+async def read_root(
+        query: Annotated[str, Path(title='Query to look up the artist by')],
+):
+    token = await spotify_token_manager.get_token()
+    async with httpx.AsyncClient() as client:
+        client.headers["Authorization"] = f"Bearer {token}"
+        response = await client.get(f'https://api.spotify.com/v1/search?q={query}&type=artist&limit=5')
 
     return response.json()
