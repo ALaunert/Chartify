5d1ace90ad94b8bd489fa83b98f8ffdc2d0041b3
ALaunert <alexmansupwork@gmail.com>
2025-06-30 12:00:45 +0200

    Added delayed search request on keyup event in searchbar

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -6,13 +6,20 @@ const query = ref("")
 const results = ref([])
 
 const getSearchData = async (search: string) => {
+  if (!search)
+  {
+    results.value = []
+
+    return;
+  }
+
   const url = `/api/artist/search/artist:${search}`
   try {
     const response = await fetch(url)
     if (!response.ok) throw new Error(`Response status: ${response.status}`)
 
     const data = await response.json()
-    results.value = data.artists.items
+    results.value = data.artists?.items || []
   }
   catch (error) {
     console.error(error)
@@ -21,14 +28,38 @@ const getSearchData = async (search: string) => {
 }
 
 const onSubmit = (e: Event) => {
-  e.preventDefault()
-  getSearchData(query.value)
-}
+  e.preventDefault();
+
+  if (debounceTimer) {
+    clearTimeout(debounceTimer);
+    debounceTimer = null;
+  }
+
+  getSearchData(query.value);
+};
+
+let debounceTimer: ReturnType<typeof setTimeout> | null = null;
+
+const ignoredKeys = new Set([
+  "Shift", "Control", "Alt", "Meta",
+  "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown",
+  "CapsLock", "Tab", "Enter", "Escape"
+]);
+
+const onKeyUp = (e: KeyboardEvent) => {
+  if (ignoredKeys.has(e.key)) return;
+
+  if (debounceTimer) clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(() => {
+    getSearchData(query.value);
+    debounceTimer = null;
+  }, 1500);
+};
 </script>
 
 <template>
   <form @submit="onSubmit" id="search-bar-form">
-    <input type="text" v-model="query" name="searchbar" id="search_bar" />
+    <input  @keyup="onKeyUp" type="text" v-model="query" name="searchbar" id="search_bar" placeholder="Enter your artist"/>
     <button type="submit" name="search">Find</button>
   </form>
 
@@ -42,5 +73,8 @@ const onSubmit = (e: Event) => {
   border-radius: 12px;
   display: flex;
   gap: 8px;
+}
+#search_bar{
+  padding-left: 8px;
 }
 </style>
